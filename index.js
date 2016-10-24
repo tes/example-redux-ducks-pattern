eec8a001309d78fe99985f10e4f27e5d81c7bbe9
Tom Ruttle <tom@tomruttle.com>
2016-10-24 15:44:35 +0900

    add flowtypes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// @flow
+
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import util from 'util';
 
@@ -13,7 +15,7 @@ const rootReducer = combineReducers({
   user,
 });
 
-const middleware = (store) => (next) => (action) => {
+const middleware = (store) => (next) => (action /* : allActionsType */) => {
   if (action.type === constants.SET_USER_NAME) {
     process.stdout.write('an action has passed through!\n');
   }
@@ -22,22 +24,32 @@ const middleware = (store) => (next) => (action) => {
 
 const store = createStore(rootReducer, initialState, applyMiddleware(middleware));
 
-const initialName = selectors.getUserName(store.getState());
+const initialName = selectors.getUserName(store.getState()) || 'NO NAME SET';
 
 process.stdout.write(`user name initiated as: ${initialName}\n`);
 
-const updateNameAndOutput = (text) => {
-  text = text.replace('\n', '');
+const updateNameAndOutput = (text /* : ?string */) => {
+  text = text ? text.replace('\n', '') : null;
 
   if (text) {
     store.dispatch(actions.setUserName(util.inspect(text)));
 
-    const updatedName = selectors.getUserName(store.getState());
+    const updatedName = selectors.getUserName(store.getState()) || 'STILL NO NAME';
 
     process.stdout.write(`user name is now: ${updatedName}\n`);
+  } else {
+    store.dispatch(actions.setUserName(123))
   }
 };
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', updateNameAndOutput);
+
+/* flow-include
+
+import type { nameType, userActionTypes } from './user';
+
+type allActionsType = userActionTypes;
+
+*/
--- user.js
+++ user.js
@@ -1,3 +1,5 @@
+// @flow
+
 import { combineReducers } from 'redux';
 import { createSelector }from 'reselect';
 
@@ -5,22 +7,29 @@ const defaultName = null;
 
 export const constants = {
   SET_USER_NAME: 'user/SET_USER_NAME',
+  RESET_USER_NAME: 'user/RESET_USER_NAME',
 };
 
 export const actions = {
-  setUserName(name) {
+  setUserName(name /* : ?string */) /* : setUserNameType */ {
     return {
       type: constants.SET_USER_NAME,
       payload: name,
     };
   },
+  resetUserName() /* : resetUserNameType */ {
+    return {
+      type: constants.RESET_USER_NAME,
+      payload: defaultName,
+    }
+  }
 };
 
 const getRootSelector = (state) => state.user;
 
 const getUserName = createSelector(
   getRootSelector,
-  ({ name }) => name
+  ({ name }) /* : nameType */ => name
 );
 
 export const selectors = {
@@ -28,10 +37,28 @@ export const selectors = {
 };
 
 export default combineReducers({
-  name: (state, action) => {
+  name: (state /* : nameType */, action /* : userActionTypes */) => {
     switch (action.type) {
-      case constants.SET_USER_NAME: return action.payload;
+      case constants.SET_USER_NAME: return action.payload || state;
       default: return state || defaultName;
     }
   },
 });
+
+/* flow-include
+
+export type nameType = string | null;
+
+type setUserNameType = {
+  type: string,
+  payload: ?string,
+};
+
+type resetUserNameType = {
+  type: string,
+  payload: null,
+};
+
+export type userActionTypes = setUserNameType | resetUserNameType;
+
+*/
